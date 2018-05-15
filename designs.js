c52868091c2c6d9d99ea461c5c5ea6bf88916edd
Leah Joyce <leahpjoyce@gmail.com>
2018-05-14 22:42:17 -0500

    new designs.js

--- designs.js
+++ designs.js
@@ -1,24 +1,24 @@
+$( "#sizePicker" ).submit(function( event ) {
 
+  event.preventDefault();
+  
+  const height = $('#inputHeight').val();
+  const width = $('#inputWidth').val();
+  makeGrid(height, width);
+});
 
-$('#sizePicker').on("click", function(event) {
-event.preventDefault();
-$('tr').remove();
-const height = $('#inputHeight').val();
-const weight = $('#inputWeight').val();
-  for (let i = 0; i < height; i++) {
-  	height[i] = $("table").append("<tr></tr>");
-  for (let j = 0; j < weight; j++) {
-    weight[j] = $("tr:last").append("<td></td>");
+function makeGrid(a, b) {
+    $('tr').remove();
+    
+    for (let i = 0; i < a; i++) {
+  	$("table").append("<tr></tr>");
+  for (let j = 0; j < b; j++) {
+    $("tr:last").append("<td></td>");
 	$('td').attr('class', 'cell')
     }
   }
-
-$(".cell").on("click", function() {
-const color = $('#colorPicker').val();
-$(this).css("background-color", color);
-});
-
-});
-
-
-
+   $(".cell").on("click", function() {
+    const color = $('#colorPicker').val();
+    $(this).css("background-color", color);
+}); 
+}
